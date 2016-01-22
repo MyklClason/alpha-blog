2d648fb46155cf44e871317ec550cb29cc2f2108
Mykl Clason <myklclason@gmail.com>
2016-01-22 05:19:50 +0000

    Add admin requirement to create categories

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -14,13 +14,6 @@ class ListCategoryTest < ActionDispatch::IntegrationTest
 
       assert_select "a[href=?]", category_path(@category), text: @category.name
       assert_select "a[href=?]", category_path(@category2), text: @category2.name
-
-
-      assert_difference 'Category.count', 1 do
-        post_via_redirect categories_path, category: {name: "sports"}
-      end
-      assert_template 'categories/index'
-      assert_match "sports", response.body
     end
 
 end
